44621175d7f0b6c1b3173337357a3215122dc3bd
hamthelegend <hamuelxd@gmail.com>
2022-07-15 12:03:47 +0800

    Added Minecraft's colors

--- app/src/main/java/com/hamthelegend/enchantmentorder/android/ui/theme/Color.kt
+++ app/src/main/java/com/hamthelegend/enchantmentorder/android/ui/theme/Color.kt
@@ -1,5 +1,7 @@
 package com.hamthelegend.enchantmentorder.android.ui.theme
 
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +11,21 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val SuperLightGreen = Color(0xFFEFF8E8)
+val LightGreen = Color(0xFF8FCA5C)
+val Green = Color(0xFF70B237)
+val SuperDarkGreen = Color(0xFF292D22)
+val LightBrown = Color(0xFFE6BB8E)
+val Brown = Color(0xFF854F2B)
+val DarkBrown = Color(0xFF3F200E)
+
+val LightColorPalette = lightColorScheme(
+    primary = Green,
+    secondary = Brown,
+)
+
+val DarkColorPalette = darkColorScheme(
+    primary = LightGreen,
+    secondary = LightBrown,
+)
