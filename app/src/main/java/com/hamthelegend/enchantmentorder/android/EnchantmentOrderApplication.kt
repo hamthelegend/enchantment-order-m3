abbd48d0faf14711e240c258854e33ca87bbd596
hamthelegend <hamuelxd@gmail.com>
2022-07-19 02:58:15 +0800

    Added Hilt

---
+++ app/src/main/java/com/hamthelegend/enchantmentorder/android/EnchantmentOrderApplication.kt
@@ -0,0 +1,7 @@
+package com.hamthelegend.enchantmentorder.android
+
+import android.app.Application
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class EnchantmentOrderApplication : Application()
