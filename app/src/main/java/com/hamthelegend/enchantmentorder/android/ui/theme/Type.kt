28dd25445d77d0e598c9126a60b43ff9db0427bc
hamthelegend <hamuelxd@gmail.com>
2022-07-21 02:47:01 +0800

    Updated Type.kt

--- app/src/main/java/com/hamthelegend/enchantmentorder/android/ui/theme/Type.kt
+++ app/src/main/java/com/hamthelegend/enchantmentorder/android/ui/theme/Type.kt
@@ -36,10 +36,24 @@ val Typography = Typography(
         letterSpacing = 0.25.sp,
     ),
     titleLarge = TextStyle(
-        fontFamily = robotoSlab,
+        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 16.sp,
         letterSpacing = 0.sp
     ),
     bodyLarge = TextStyle(
